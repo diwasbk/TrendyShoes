71b795635d4a4a5307df50d7ef0b1d125cf8466f
diwasbk <diwasbk903@gmail.com>
2025-02-03 16:24:28 +0545

    Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,26 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import AppLayout from "./components/public/AppLayout"
+import Home from "./components/public/Home"
+import Product from "./components/public/Product"
+import About from "./components/public/About"
+import Contact from "./components/public/Contact"
 
 function App() {
 
   return (
     <>
-      
+    <BrowserRouter>
+      <Routes>
+        {/* Public routes */}
+        <Route path="/" element={<AppLayout/>}>
+          <Route index element={<Home/>}/>
+          <Route path="home" element={<Home/>}/>
+          <Route path="products" element={<Product/>}/>
+          <Route path="about" element={<About/>}/>
+          <Route path="contact" element={<Contact/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
---
+++ frontend/src/components/public/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav className="w-full h-22 text-white bg-black flex items-center justify-between px-4">
+      <h1 className="text-4xl font-bold hover:text-red-400 ml-2"><Link to="/home">TrendyShoes</Link></h1>
+      <div className="flex-grow flex justify-end">
+        <ul className="hidden md:flex text-[22px] justify-evenly w-full max-w-4xl">
+          <li className="mx-[25px] cursor-pointer flex items-center justify-center">
+            <Link to="/home" className="hover:text-red-400">Home</Link>
+          </li>
+          <li className="mx-[25px] cursor-pointer flex items-center justify-center">
+            <Link to="/products" className="hover:text-red-400">Products</Link>
+          </li>
+          <li className="mx-[25px] cursor-pointer flex items-center justify-center">
+            <Link to="/about" className="hover:text-red-400">About</Link>
+          </li>
+          <li className="mx-[25px] cursor-pointer flex items-center justify-center">
+            <Link to="/contact" className="hover:text-red-400">Contact</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
